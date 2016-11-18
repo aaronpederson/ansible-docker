bbbf0d7c9464264627d6724bf926fb6d154ef130
alexandarp <alexandar.podobnik@gmail.com>
2016-11-18 11:54:06 -0500

    IO-621 updating package name to docker-engine

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'ansible-docker::default' do
 
-  describe package('docker') do
+  describe package('docker-engine') do
     it { should be_installed.by('apt') }
   end
 
